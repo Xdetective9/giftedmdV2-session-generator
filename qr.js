94df7ee842f1c8f897fe731412a30addf635bff0
Xdetective9 <158455774+Xdetective9@users.noreply.github.com>
2025-08-15 08:55:10 +0500

    Update qr.js

--- qr.js
+++ qr.js
@@ -14,17 +14,17 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN GIFTED-MDv2 』*
+║ *『 WOW YOU'VE CHOSEN Xdetective!🔥 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@giftedtechnexus_
-║❒ *Owner:* _https://wa.me/message/NHCZC5DSOEUXB1_
-║❒ *Repo:* _https://github.com/Giftedmaurice/gifted-mdV2_
-║❒ *WaGroup:* _https://chat.whatsapp.com/L0ctUUVLlsrFYwBHApKfew_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o_
-║❒ *Plugins:* _https://github.com/Giftedmaurice/gifted-bot-md-plugins_
+║❒ *Ytube:* https://youtube.com/@ranaabdullah-b7r?si=iZSZQaoEKVpFZCt3
+║❒ *Owner:* _https://wa.me/message/NHCZC5DSOEUXB1_923288055104
+║❒ *Repo:* _https://github.com/Xdetevtive9/xdetective
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029VbAmCGa0AgW9oyj1PT1V
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbAmCGa0AgW9oyj1PT1V
+║❒ 
 ╚════════════════════════╝
 
 `
@@ -37,7 +37,7 @@ if (fs.existsSync('./auth_info_baileys')) {
   
   app.use("/", async(req, res) => {
 
-  const { default: GiftedWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const { default: XdetectiveWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
   const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
   async function GIFTED() {
     const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
@@ -121,7 +121,7 @@ SESSION-ID ==> ${Scan_Id}
     await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
 
 
-    //// MADE WITH GIFTED TECH
+    //// MADE WITH Xdetective!🔥
 
 });
 
